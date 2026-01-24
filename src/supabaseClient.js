1f4717b0ee17b467fa3240014f14693f919a3f59
Rodrigo <rodrigoss1053@gmail.com>
2026-01-24 01:48:05 -0300

    corrigindo conexao

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// URL correta (ajustada sem o ponto extra no final)
+const supabaseUrl = 'https://vqpboodhhyvwtfrpgrk.supabase.co';
+
+// Chave Anon Public (com as aspas obrigatórias)
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxcGJib2RoaHl2d3RmdnJwZ3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMTE3NDksImV4cCI6MjA4NDc4Nzc0OX0.8Swb8y8YbzTtYuAEc9flAYyIGiYo5fNAqPQJvWqrZEs';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
